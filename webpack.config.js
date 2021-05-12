6f25cc63b28ecdf1fc97176985bd0547dd18d739
tanbo <464328895@qq.com>
2021-05-13 00:16:45 +0800

    feat: 拆分仓库

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const ip = require('ip');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: {
+    index: path.resolve(__dirname, 'index.ts')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devServer: {
+    host: 'localhost',
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true,
+    open: true
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      enforce: 'pre',
+      exclude: /node_modules/,
+      use: [{
+        loader: 'eslint-loader'
+      }]
+    }, {
+      test: /\.tsx?$/,
+      use: ['ts-loader']
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+};
